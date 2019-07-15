ab71a76bdb31f07d410710c88c21831173c930c7
Jake M <scudsterr@gmail.com>
2019-07-15 10:15:31 -0400

    Update script.js
    Added more comments

--- js/script.js
+++ js/script.js
@@ -6,35 +6,16 @@ FSJS project 2 - List Filter and Pagination
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 
-/***
-   Add your global variables that store the DOM elements you will
-   need to reference and/or manipulate.
+//Global variables for list items and number of items per page
 
-   But be mindful of which variables should be global and which
-   should be locally scoped to one of the two main functions you're
-   going to create. A good general rule of thumb is if the variable
-   will only be used inside of a function, then it can be locally
-   scoped to that function.
-***/
 const listItems = document.getElementsByClassName('student-item cf');
 const itemsPerPage = 10
 
 
 
-/***
-   Create the `showPage` function to hide all of the items in the
-   list except for the ten you want to show.
 
-   Pro Tips:
-     - Keep in mind that with a list of 54 students, the last page
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when
-       you initially define the function, and it acts as a variable
-       or a placeholder to represent the actual function `argument`
-       that will be passed into the parens later when you call or
-       "invoke" the function
-***/
+
+//Function to limit the number of page items shown to 10.
 function showPage(list, page) {
   const startIndex = (page * itemsPerPage) - itemsPerPage;
   const endIndex = page * itemsPerPage
@@ -46,6 +27,10 @@ function showPage(list, page) {
     }
   }
 }
+//Initial function call to display webpage
+
+showPage(listItems, 1);
+
 //Function to add page links at the bottom of the webpage
 
 function appendPageLinks(list){
@@ -58,12 +43,12 @@ function appendPageLinks(list){
   div.appendChild(ul);
 
   //For loop to create button based on the number of pages.  It then creates an li and anchor element for each page
-  for (let i = 0; i <= numOfPages; i++) {
+  for (let i = 1; i <= numOfPages; i++) {
     const li = document.createElement('li');
     ul.appendChild(li);
     const a = document.createElement('a');
     a.href = '#'
-    a.textContent = list[i]
+    a.textContent = i
     a[0].className = 'active';
     li.appendChild(a);
 
@@ -71,16 +56,15 @@ function appendPageLinks(list){
     a.addEventListener('click', (e) => {
       e.target.className = 'active'
       for (let i = 0; i < numOfPages; i++) {
-        a.className = '';
+        a[i].className = '';
       }
       showPage(listItems, e.textContent)
     });
 
-
   }
 }
 
-showPage(listItems, 1);
+
 
 appendPageLinks(listItems);
 
