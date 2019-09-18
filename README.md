e826903d1300debbd211b2a0ca4a7478dc4d3155
Jake M <scudsterr@gmail.com>
2019-09-18 11:54:06 -0400

    Refactoring
    Refactoring for better readability

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Techdegree-Project-2
 This website will accept any number of users and divide them up into 10 users per page, with clickable numbered links at the bottom to go to different pages.
+
+A search bar was added to the webpage to make finding students easier, it dynamically changes the pagination links based on the number of ressults returned.
--- js/script.js
+++ js/script.js
@@ -26,26 +26,27 @@ showPage(listItems, 1);
 
 //Function to add page links at the bottom of the webpage
 function appendPageLinks(list){
-
   const numOfPages = Math.ceil(list.length / itemsPerPage);
   const div = document.createElement('div')
   let pageDiv = document.querySelector('.page');
-  div.className = 'pagination';
   let paginationClass = document.querySelector('.pagination');
-  pageDiv.appendChild(div);
   const ul = document.createElement('ul');
+  div.className = 'pagination';
   ul.className = 'exists';
 
-//Trying to erase ul elements if they already exist
+  //Erasing the ul elements if they already exist
   if (document.body.contains(document.querySelector('.exists'))) {
     paginationClass.remove(document.querySelector('.exists'));
   }
+
+  pageDiv.appendChild(div);
   div.appendChild(ul);
+
+
 
   //For loop to create button based on the number of pages.  It then creates an li and anchor element for each page
   for (let i = 1; i <= numOfPages; i++) {
     const li = document.createElement('li');
-    ul.appendChild(li);
     const a = document.createElement('a');
     a.href = '#'
     a.textContent = i
@@ -53,18 +54,18 @@ function appendPageLinks(list){
     if (i === 1) {
       a.className = 'active';
     }
+    ul.appendChild(li);
     li.appendChild(a);
 
     //Event Listener to go to the index page that is clicked and to remove the active class from all other anchor elements
     a.addEventListener('click', (e) => {
       let activeClass = document.querySelector(".active");
       activeClass.classList.remove("active");
-
       //Adds active class to the target
       e.target.className = 'active'
 
-    // Call showPage to display the page clicked
-    showPage(list, e.target.textContent);
+      // Call showPage to display the page clicked
+      showPage(list, e.target.textContent);
     });
   }
 }
@@ -74,19 +75,18 @@ appendPageLinks(listItems);
 
 //Creating the HTML for where the searchBar will be placed
 const searchDiv = document.createElement('div');
-searchDiv.className = 'student-search';
 const divHeader = document.getElementsByClassName('page-header cf')[0];
-divHeader.appendChild(searchDiv);
 const searchInput = document.createElement('input');
+const searchSubmitButton = document.createElement('button');
+const noResults = document.createElement('h3');
+searchDiv.className = 'student-search';
 searchInput.placeholder = 'Search for students...';
 searchInput.className = 'search-input'
-searchDiv.appendChild(searchInput);
-const searchSubmitButton = document.createElement('button');
 searchSubmitButton.textContent = 'Search';
-searchDiv.appendChild(searchSubmitButton);
-const noResults = document.createElement('h3');
 noResults.id = "padding"
-
+divHeader.appendChild(searchDiv);
+searchDiv.appendChild(searchInput);
+searchDiv.appendChild(searchSubmitButton);
 
 //Function to compare search input to the list of names
 function searchBar(searchInput, list) {
@@ -100,6 +100,8 @@ function searchBar(searchInput, list) {
     //Conditional to find the name from the list of names
     if (list[i].children[0].children[1].textContent.toLowerCase().includes(searchInput.value.toLowerCase())) {
       matchArray.push(list[i]);
+      //Erase no results if there is a matching name
+      noResults.textContent = '';
     }
   //Conditional to say No results if the name is not in the list
   }
